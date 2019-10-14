87682e16ee73d5f5b0126ead9fab97421bbed797
vihong <probamaths@gmail.com>
2019-10-15 01:20:21 +0200

    added js animations.js

---
+++ js/animations.js
@@ -0,0 +1,26 @@
+
+const ratio  = 0.1;
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+
+  const handleIntersect = function(entries, observer) { 
+    entries.forEach(entry => {
+    if (entry.intersectionRatio > ratio){
+        console.log("element visible à " + entry.intersectionRatio)
+        entry.target.classList.add('reveal-visible')
+        observer.unobserve(entry.target)
+    }
+    });
+};
+
+
+
+  const observer = new IntersectionObserver(handleIntersect, options)
+  document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
+    observer.observe(r);
+  })
+    
